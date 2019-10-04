46c8a6302a67daf47391691e723e624a863d0564
Josh Tozer <joshiantozer@gmail.com>
2019-10-04 14:11:51 -0600

    Read all data into program

--- tozerwillson_project2.m
+++ tozerwillson_project2.m
@@ -1,1 +1,56 @@
 % Project 2
+
+% line 1 data
+data1 = dlmread('line1-1.txt');
+
+target_box1 = data1(1:2);
+
+box_size1 = data1(3);
+
+point1 = data1(4:5);
+
+vec1 = data1(6:7);
+
+% line 2 data
+data2 = dlmread('line2.txt');
+
+target_box2 = data2(1:2);
+
+box_size2 = data2(3);
+
+point2 = data2(4:5);
+
+vec2 = data2(6:7);
+
+% line 3 data
+data3 = dlmread('line3.txt');
+
+target_box3 = data3(1:2);
+
+box_size3 = data3(3);
+
+point3 = data3(4:5);
+
+vec3 = data3(6:7);
+
+% line 4 data
+data4 = dlmread('line4.txt');
+
+target_box4 = data4(1:2);
+
+box_size4 = data4(3);
+
+point4 = data4(4:5);
+
+vec4 = data4(6:7);
+
+% line 5 data
+data5 = dlmread('line5.txt');
+
+target_box5 = data5(1:2);
+
+box_size5 = data5(3);
+
+point5 = data5(4:5);
+
+vec5 = data5(6:7);
