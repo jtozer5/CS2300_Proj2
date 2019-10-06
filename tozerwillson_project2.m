65f51acd5b9a9766d29c05ee336a90aec68670e9
willsonnathan <55596971+willsonnathan@users.noreply.github.com>
2019-10-06 14:36:21 -0600

    Update tozerwillson_project2.m
    Try making a 6th text file with the values given in the example in the project description. The answer looks similar to theirs, but I think there is an issue in line 26. I feel like the line is shifted up just slightly in the graph.

--- tozerwillson_project2.m
+++ tozerwillson_project2.m
@@ -1,20 +1,32 @@
-% Project 2
+%{
+Josh Tozer & Nathan Willson
+CS 2300 002
+Project 2
+Fri Oct 11, 2019 at 11:59 pm
+This assignment will use a file that contains target box lower left coordinate, 
+target box size, and information describing a line in parametric form. Using this 
+information, we will plot the line using character output and will create a target
+box from the implicit form of the line. We will do this graphically.
+%}
 
-% line data
+%Outputs name and assignment number.
+disp('Josh Tozer & Nathan Willson')
+disp('Project 2')
+
+%Gets the numbers from the text file and saves the target box lower left coordinate, 
+%target box size, and information describing a line in parametric form.
 data = dlmread('line3.txt');
 target_box = data(1:2);
 box_size = data(3);
 point = data(4:5);
 vec = data(6:7);
 
-% Draw target box
-x1 = target_box(1);
-x2 = target_box(1) + box_size;
-y1 = target_box(2);
-y2 = target_box(2) + box_size;
-x = [x1, x2, x2, x1, x1];
-y = [y1, y1, y2, y2, y1];
-plot(x, y, 'b-');
+%Uses the parametric equation of a line to display the line to the user.
 hold on;
+t =linspace(-box_size/2, box_size/2);
+v = point(1) + t * vec(1);
+w = point(2) + t * vec(2);
+plot(w,v, 'Color', 'b', 'LineStyle', ':');
 
-% Draw line
+%Creates a rectangle that displays the target box.
+rectangle('Position',[target_box(1), target_box(2), box_size, box_size], 'EdgeColor','r', 'LineStyle','--');
